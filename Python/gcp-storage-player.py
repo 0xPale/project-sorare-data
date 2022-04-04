8b52b31d881e153b79c50a668108bf6cdc1ae178
scapeshiftX <88829372+scapeshiftX@users.noreply.github.com>
2022-04-04 23:58:47 +0200

    refactor with a loop

--- Python/gcp-storage-player.py
+++ Python/gcp-storage-player.py
@@ -1,8 +1,9 @@
 import os
 import getpass
+import glob
 
 from variables import outputFolderLocal, outputFolderCloud,\
-    outputCSVPlayer, googleAppCredentialsLocal, googleAppCredentialsCloud
+    outputCSVPlayer, googleAppCredentialsLocal, googleAppCredentialsCloud, bucket_name
 
 from functions import upload_blob
 
@@ -17,11 +18,6 @@ else:
 # This is a way to set the environment variable for the Google Cloud API.
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= googleAppCredentials
 
-
-bucket_name = "sorare-data"
-
-# Uploading the files to the cloud.
-upload_blob(bucket_name, outputFolder + outputCSVPlayer + "player.csv", "player.csv")
-upload_blob(bucket_name, outputFolder + outputCSVPlayer + "allSo5Scores.csv", "allSo5Scores.csv")
-#upload_blob(bucket_name, outputFolder + outputCSVPlayer + "lastSo5Scores.csv", "lastSo5Scores.csv")
-upload_blob(bucket_name, outputFolder + outputCSVPlayer + "cardSupply.csv", "cardSupply.csv")
+# A way to upload the files to the Google Cloud Storage.
+for f in glob.glob(outputFolder + outputCSVPlayer + "*.csv"):
+    upload_blob(bucket_name, f, os.path.basename(f))
