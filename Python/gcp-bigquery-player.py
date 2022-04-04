c6c9cc8bd7992a110e1c3ef7a3a1c9fd67111ba6
scapeshiftX <88829372+scapeshiftX@users.noreply.github.com>
2022-04-04 13:25:52 +0200

    fix

--- Python/gcp-bigquery-player.py
+++ Python/gcp-bigquery-player.py
@@ -1,29 +1,14 @@
 from google.cloud import bigquery
-from google.cloud import storage
 import os
 import getpass
 
-from variables import outputFolderLocal, outputFolderCloud, googleAppCredentialsCloud,\
-    outputCSVPlayer, googleAppCredentialsLocal, googleAppCredentialsCloud
-
-from functions import upload_blob
+from variables import googleAppCredentialsCloud, googleAppCredentialsLocal
 
 # This is a way to set the environment variable for the Google Cloud API.
 if getpass.getuser() == "benjamin":
     googleAppCredentials = googleAppCredentialsLocal
-    outputFolder = outputFolderLocal
 else:
     googleAppCredentials = googleAppCredentialsCloud
-    outputFolder = outputFolderCloud
-
-# This is a way to set the environment variable for the Google Cloud API.
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= googleAppCredentials
-
-# Uploading the files to the cloud.
-upload_blob("sorare-data", outputFolder + outputCSVPlayer + "player.csv", "player.csv")
-upload_blob("sorare-data", outputFolder + outputCSVPlayer + "allSo5Scores.csv", "allSo5Scores.csv")
-upload_blob("sorare-data", outputFolder + outputCSVPlayer + "cardSupply.csv", "cardSupply.csv")
-
 
 ########################################################################################################################
 # Construct a BigQuery client object.
