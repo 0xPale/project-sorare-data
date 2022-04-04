3ec2e24aff25a4a150989c8e5194dda1079b0d67
scapeshiftX <88829372+scapeshiftX@users.noreply.github.com>
2022-04-04 23:35:17 +0200

    add function for list blob and move blob

--- Python/functions.py
+++ Python/functions.py
@@ -1,6 +1,86 @@
 from google.cloud import bigquery
 from google.cloud import storage
 
+###############################################################################
+
+def list_blobs_with_prefix(bucket_name, prefix, delimiter=None):
+    """Lists all the blobs in the bucket that begin with the prefix.
+
+    This can be used to list all blobs in a "folder", e.g. "public/".
+
+    The delimiter argument can be used to restrict the results to only the
+    "files" in the given "folder". Without the delimiter, the entire tree under
+    the prefix is returned. For example, given these blobs:
+
+        a/1.txt
+        a/b/2.txt
+
+    If you specify prefix ='a/', without a delimiter, you'll get back:
+
+        a/1.txt
+        a/b/2.txt
+
+    However, if you specify prefix='a/' and delimiter='/', you'll get back
+    only the file directly under 'a/':
+
+        a/1.txt
+
+    As part of the response, you'll also get back a blobs.prefixes entity
+    that lists the "subfolders" under `a/`:
+
+        a/b/
+    """
+
+    storage_client = storage.Client()
+
+    # Note: Client.list_blobs requires at least package version 1.17.0.
+    blobs = storage_client.list_blobs(bucket_name, prefix=prefix, delimiter=delimiter)
+
+    print("Blobs:")
+    for blob in blobs:
+        print(blob.name)
+
+    if delimiter:
+        print("Prefixes:")
+        for prefix in blobs.prefixes:
+            print(prefix)
+    
+    return(blobs)
+
+
+def move_blob(bucket_name, blob_name, destination_bucket_name, destination_blob_name):
+    """Moves a blob from one bucket to another with a new name."""
+    # The ID of your GCS bucket
+    # bucket_name = "your-bucket-name"
+    # The ID of your GCS object
+    # blob_name = "your-object-name"
+    # The ID of the bucket to move the object to
+    # destination_bucket_name = "destination-bucket-name"
+    # The ID of your new GCS object (optional)
+    # destination_blob_name = "destination-object-name"
+
+    storage_client = storage.Client()
+
+    source_bucket = storage_client.bucket(bucket_name)
+    source_blob = source_bucket.blob(blob_name)
+    destination_bucket = storage_client.bucket(destination_bucket_name)
+
+    blob_copy = source_bucket.copy_blob(
+        source_blob, destination_bucket, destination_blob_name
+    )
+    source_bucket.delete_blob(blob_name)
+
+    print(
+        "Blob {} in bucket {} moved to blob {} in bucket {}.".format(
+            source_blob.name,
+            source_bucket.name,
+            blob_copy.name,
+            destination_bucket.name,
+        )
+    )
+
+###############################################################################
+
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
     """Uploads a file to the bucket."""
     # The ID of your GCS bucket
@@ -22,6 +102,7 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
         )
     )
 
+###############################################################################
 
 def load_csv_file(uri, table_id, job_config):
     """Load a csv to google big query."""
