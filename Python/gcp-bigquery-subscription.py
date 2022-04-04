5b6c8e4337eeb987f0367d36df3a64b1035d3fe2
scapeshiftX <88829372+scapeshiftX@users.noreply.github.com>
2022-04-05 00:26:41 +0200

    finish gcp subscription

---
+++ Python/gcp-bigquery-subscription.py
@@ -0,0 +1,62 @@
+from google.cloud import bigquery
+import os
+import getpass
+
+from variables import googleAppCredentialsCloud, googleAppCredentialsLocal, bucket_name, database_name, schema_name
+from functions import load_csv_file, list_blobs_with_prefix
+
+# This is a way to set the environment variable for the Google Cloud API.
+if getpass.getuser() == "benjamin":
+    googleAppCredentials = googleAppCredentialsLocal
+else:
+    googleAppCredentials = googleAppCredentialsCloud
+
+# This is a way to set the environment variable for the Google Cloud API.
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= googleAppCredentials
+
+########################################################################################################################
+
+# Sorting the list of blobs by name.
+blobs = list_blobs_with_prefix(bucket_name, prefix="subscription")
+blobs.sort()
+
+# Defining the location of the files to be loaded.
+uri_card = "gs://" + bucket_name + "/" + blobs[0] #subscriptionCard_DATETIME.csv
+uri_transfer = "gs://" + bucket_name + "/" + blobs[1] #subscriptionTransfer_DATETIME.csv
+
+# A way to set the name of the table.
+table_id_card = database_name + "." + schema_name + "." + "card"
+table_id_transfer = database_name + "." + schema_name + "." + "transfer"
+
+# Setting the parameters for the load job.
+job_config_card = bigquery.LoadJobConfig(
+    autodetect=True,
+    field_delimiter=";",
+    skip_leading_rows=1,
+    write_disposition=bigquery.WriteDisposition.WRITE_APPEND,
+    # The source format defaults to CSV, so the line below is optional.
+    source_format=bigquery.SourceFormat.CSV,
+)
+
+job_config_transfer = bigquery.LoadJobConfig(
+    schema=[
+        bigquery.SchemaField("card_slug", "STRING"),
+        bigquery.SchemaField("manager_slug", "STRING"),
+        bigquery.SchemaField("manager_nickname", "STRING"),
+        bigquery.SchemaField("transfer_date", "TIMESTAMP"),
+        bigquery.SchemaField("transfer_type", "STRING"),
+        bigquery.SchemaField("transfer_priceETH", "FLOAT"),
+        bigquery.SchemaField("transfer_priceFiat_usd", "FLOAT"),
+        bigquery.SchemaField("extracted_at", "TIMESTAMP"),
+    ],
+    field_delimiter=";",
+    skip_leading_rows=1,
+    write_disposition=bigquery.WriteDisposition.WRITE_APPEND,
+    # The source format defaults to CSV, so the line below is optional.
+    source_format=bigquery.SourceFormat.CSV,
+)
+
+#Card csv loading
+load_csv_file(uri_card, table_id_card, job_config_card)
+#Transfer csv loading
+load_csv_file(uri_card, table_id_transfer, job_config_transfer)
--- Python/gcp-storage-subscription.py
+++ Python/gcp-storage-subscription.py
@@ -1,10 +1,11 @@
 import os
 import getpass
+import glob
 
 from variables import outputFolderLocal, outputFolderCloud,\
-    outputCSVPlayer, googleAppCredentialsLocal, googleAppCredentialsCloud
+    outputCSVSubscription, googleAppCredentialsLocal, googleAppCredentialsCloud, bucket_name
 
-from functions import upload_blob
+from functions import list_blobs_with_prefix, move_blob, upload_blob
 
 # This is a way to set the environment variable for the Google Cloud API.
 if getpass.getuser() == "benjamin":
@@ -17,10 +18,10 @@ else:
 # This is a way to set the environment variable for the Google Cloud API.
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= googleAppCredentials
 
+# Moving the files from the subscription folder to the subscription-done folder.
+for blob in list_blobs_with_prefix(bucket_name, prefix="subscription"):
+    move_blob(bucket_name, blob.name, bucket_name + "done/" + blob.name)
 
-bucket_name = "sorare-data"
-
-# Uploading the files to the cloud.
-upload_blob(bucket_name, outputFolder + outputCSVPlayer + "player.csv", "player.csv")
-upload_blob(bucket_name, outputFolder + outputCSVPlayer + "allSo5Scores.csv", "allSo5Scores.csv")
-upload_blob(bucket_name, outputFolder + outputCSVPlayer + "cardSupply.csv", "cardSupply.csv")
+# A way to upload the files to the Google Cloud Storage.
+for f in glob.glob(outputFolder + outputCSVSubscription + "*.csv"):
+    upload_blob(bucket_name, f, os.path.basename(f))
