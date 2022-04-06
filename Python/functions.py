1fe6d6257dc30f87eb09594638f441ae1e4a8ea0
scapeshiftX <88829372+scapeshiftX@users.noreply.github.com>
2022-04-06 11:44:56 +0200

    On vire le print car ça causait des problèmes de iterator has already started

--- Python/functions.py
+++ Python/functions.py
@@ -36,14 +36,14 @@ def list_blobs_with_prefix(bucket_name, prefix, delimiter=None):
     # Note: Client.list_blobs requires at least package version 1.17.0.
     blobs = storage_client.list_blobs(bucket_name, prefix=prefix, delimiter=delimiter)
 
-    print("Blobs:")
-    for blob in blobs:
-        print(blob.name)
+    # print("Blobs:")
+    # for blob in blobs:
+    #     print(blob.name)
 
-    if delimiter:
-        print("Prefixes:")
-        for prefix in blobs.prefixes:
-            print(prefix)
+    # if delimiter:
+    #     print("Prefixes:")
+    #     for prefix in blobs.prefixes:
+    #         print(prefix)
     
     return(blobs)
 
