d7d89a54f193851dfb413339f86cffe4fe918ae1
scapeshiftX <88829372+scapeshiftX@users.noreply.github.com>
2022-04-06 18:53:06 +0200

    refactor storage and bigquery in only one file in order to have only 5 CRON jobs for cronitor monitoring free tiers

---
+++ Python/gcp-player.py
@@ -0,0 +1,122 @@
+from google.cloud import bigquery
+import os
+import getpass
+import glob
+
+from variables import outputFolderLocal, outputFolderCloud, outputCSVPlayer, \
+    googleAppCredentialsLocal, googleAppCredentialsCloud, \
+    bucket_name, database_name, schema_name
+
+from functions import upload_blob, load_csv_file
+
+# This is a way to set the environment variable for the Google Cloud API.
+if getpass.getuser() == "benjamin":
+    googleAppCredentials = googleAppCredentialsLocal
+    outputFolder = outputFolderLocal
+else:
+    googleAppCredentials = googleAppCredentialsCloud
+    outputFolder = outputFolderCloud
+
+# This is a way to set the environment variable for the Google Cloud API.
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= googleAppCredentials
+
+# A way to upload the files to the Google Cloud Storage.
+for f in glob.glob(outputFolder + outputCSVPlayer + "*.csv"):
+    upload_blob(bucket_name, f, os.path.basename(f))
+
+
+########################################################################################################################
+
+uri_player = "gs://" + bucket_name + "/" + "player.csv"
+uri_cardSupply = "gs://" + bucket_name + "/" + "cardSupply.csv"
+uri_score = "gs://" + bucket_name + "/" + "allSo5Scores.csv"
+uri_lastScore = "gs://" + bucket_name + "/" + "lastSo5Scores.csv"
+
+# A way to set the name of the table.
+table_id_player = database_name + "." + schema_name + "." + "player"
+table_id_cardSupply = database_name + "." + schema_name + "." + "cardSupply"
+table_id_score = database_name + "." + schema_name + "." + "score"
+
+# Setting the parameters for the load job.
+job_config_player = bigquery.LoadJobConfig(
+    autodetect=True,
+    field_delimiter=";",
+    skip_leading_rows=1,
+    write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+    # The source format defaults to CSV, so the line below is optional.
+    source_format=bigquery.SourceFormat.CSV,
+)
+
+job_config_cardSupply = bigquery.LoadJobConfig(
+    autodetect=True,
+    field_delimiter=";",
+    skip_leading_rows=1,
+    write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+    # The source format defaults to CSV, so the line below is optional.
+    source_format=bigquery.SourceFormat.CSV,
+)
+
+job_config_score = bigquery.LoadJobConfig(
+    schema=[
+        bigquery.SchemaField("player_slug", "STRING"),
+        bigquery.SchemaField("game_fixture_slug", "STRING"),
+        bigquery.SchemaField("game_fixture_gameWeek", "FLOAT"),
+        bigquery.SchemaField("game_fixture_eventType", "STRING"),
+        bigquery.SchemaField("game_date", "TIMESTAMP"),
+        bigquery.SchemaField("game_competition_slug", "STRING"),
+        bigquery.SchemaField("game_homeTeam_slug", "STRING"),
+        bigquery.SchemaField("game_homeGoals", "FLOAT"),
+        bigquery.SchemaField("game_awayTeam_slug", "STRING"),
+        bigquery.SchemaField("game_awayGoals", "FLOAT"),
+        bigquery.SchemaField("gameweek_score", "FLOAT"),
+        bigquery.SchemaField("decisive_score", "FLOAT"),
+        bigquery.SchemaField("category", "STRING"),
+        bigquery.SchemaField("stat", "STRING"),
+        bigquery.SchemaField("statValue", "FLOAT"),
+        bigquery.SchemaField("points", "FLOAT"),
+        bigquery.SchemaField("score", "FLOAT"),
+        bigquery.SchemaField("extracted_at", "TIMESTAMP"),
+    ],
+    field_delimiter=";",
+    skip_leading_rows=1,
+    write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
+    # The source format defaults to CSV, so the line below is optional.
+    source_format=bigquery.SourceFormat.CSV,
+)
+
+job_config_lastScore = bigquery.LoadJobConfig(
+    schema=[
+        bigquery.SchemaField("player_slug", "STRING"),
+        bigquery.SchemaField("game_fixture_slug", "STRING"),
+        bigquery.SchemaField("game_fixture_gameWeek", "FLOAT"),
+        bigquery.SchemaField("game_fixture_eventType", "STRING"),
+        bigquery.SchemaField("game_date", "TIMESTAMP"),
+        bigquery.SchemaField("game_competition_slug", "STRING"),
+        bigquery.SchemaField("game_homeTeam_slug", "STRING"),
+        bigquery.SchemaField("game_homeGoals", "FLOAT"),
+        bigquery.SchemaField("game_awayTeam_slug", "STRING"),
+        bigquery.SchemaField("game_awayGoals", "FLOAT"),
+        bigquery.SchemaField("gameweek_score", "FLOAT"),
+        bigquery.SchemaField("decisive_score", "FLOAT"),
+        bigquery.SchemaField("category", "STRING"),
+        bigquery.SchemaField("stat", "STRING"),
+        bigquery.SchemaField("statValue", "FLOAT"),
+        bigquery.SchemaField("points", "FLOAT"),
+        bigquery.SchemaField("score", "FLOAT"),
+        bigquery.SchemaField("extracted_at", "TIMESTAMP"),
+    ],
+    field_delimiter=";",
+    skip_leading_rows=1,
+    write_disposition=bigquery.WriteDisposition.WRITE_APPEND,
+    # The source format defaults to CSV, so the line below is optional.
+    source_format=bigquery.SourceFormat.CSV,
+)
+
+#Player csv loading
+load_csv_file(uri_player, table_id_player, job_config_player)
+#cardSupply csv loading
+load_csv_file(uri_cardSupply, table_id_cardSupply, job_config_cardSupply)
+#Scores csv loading
+#load_csv_file(uri_score, table_id_score, job_config_score)
+#Last Scores csv loading
+load_csv_file(uri_lastScore, table_id_score, job_config_lastScore)
---
+++ Python/gcp-subscription.py
@@ -0,0 +1,108 @@
+from google.cloud import bigquery
+import os
+import getpass
+import glob
+
+from variables import outputFolderLocal, outputFolderCloud, outputCSVSubscription, \
+    googleAppCredentialsLocal, googleAppCredentialsCloud,\
+    bucket_name, database_name, schema_name
+
+from functions import list_blobs_with_prefix, move_blob, upload_blob, load_csv_file
+
+# This is a way to set the environment variable for the Google Cloud API.
+if getpass.getuser() == "benjamin":
+    googleAppCredentials = googleAppCredentialsLocal
+    outputFolder = outputFolderLocal
+else:
+    googleAppCredentials = googleAppCredentialsCloud
+    outputFolder = outputFolderCloud
+
+# This is a way to set the environment variable for the Google Cloud API.
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"]= googleAppCredentials
+
+try:
+    # Moving the files from the subscription folder to the subscription-done folder.
+    for blob in list_blobs_with_prefix(bucket_name, prefix="subscription"):
+        move_blob(bucket_name, blob.name, bucket_name + "done/" + blob.name)
+except:
+    print("No subscription files to move.")
+
+# A way to upload the files to the Google Cloud Storage.
+for f in glob.glob(outputFolder + outputCSVSubscription + "*.csv"):
+    upload_blob(bucket_name, f, os.path.basename(f))
+
+########################################################################################################################
+
+# Sorting the list of blobs by name.
+blobs = list_blobs_with_prefix(bucket_name, prefix="subscription")
+#blobs.sort() #AttributeError: 'HTTPIterator' object has no attribute 'sort'
+
+# A way to select the right files to be loaded.
+for blob in blobs:
+    if "subscriptionCard" in blob.name:
+        blob_card = blob.name
+    elif "subscriptionTransfer" in blob.name:
+        blob_transfer = blob.name
+    else:
+        pass
+
+# Defining the location of the files to be loaded.
+try:
+    uri_card = "gs://" + bucket_name + "/" + blob_card #subscriptionCard_DATETIME.csv
+except:
+    pass
+try:
+    uri_transfer = "gs://" + bucket_name + "/" + blob_transfer #subscriptionTransfer_DATETIME.csv
+except:
+    pass
+
+# A way to set the name of the table.
+table_id_card = database_name + "." + schema_name + "." + "card"
+table_id_transfer = database_name + "." + schema_name + "." + "transfer"
+
+# Setting the parameters for the load job.
+job_config_card = bigquery.LoadJobConfig(
+    schema=[
+        bigquery.SchemaField("card_slug", "STRING"),
+        bigquery.SchemaField("card_name", "STRING"),
+        bigquery.SchemaField("player_slug", "STRING"),
+        bigquery.SchemaField("card_rarity", "STRING"),
+        bigquery.SchemaField("card_season_startYear", "INTEGER"),
+        bigquery.SchemaField("extracted_at", "TIMESTAMP"),
+    ],
+    field_delimiter=";",
+    skip_leading_rows=1,
+    write_disposition=bigquery.WriteDisposition.WRITE_APPEND,
+    # The source format defaults to CSV, so the line below is optional.
+    source_format=bigquery.SourceFormat.CSV,
+)
+
+job_config_transfer = bigquery.LoadJobConfig(
+    schema=[
+        bigquery.SchemaField("card_slug", "STRING"),
+        bigquery.SchemaField("manager_slug", "STRING"),
+        bigquery.SchemaField("manager_nickname", "STRING"),
+        bigquery.SchemaField("transfer_date", "TIMESTAMP"),
+        bigquery.SchemaField("transfer_type", "STRING"),
+        bigquery.SchemaField("transfer_priceETH", "FLOAT"),
+        bigquery.SchemaField("transfer_priceFiat_usd", "FLOAT"),
+        bigquery.SchemaField("extracted_at", "TIMESTAMP"),
+    ],
+    field_delimiter=";",
+    skip_leading_rows=1,
+    write_disposition=bigquery.WriteDisposition.WRITE_APPEND,
+    # The source format defaults to CSV, so the line below is optional.
+    source_format=bigquery.SourceFormat.CSV,
+)
+
+#Card csv loading
+# This is a way to avoid errors when the files are not available.
+try:
+    load_csv_file(uri_card, table_id_card, job_config_card)
+except:
+    pass
+#Transfer csv loading
+try:
+    load_csv_file(uri_transfer, table_id_transfer, job_config_transfer)
+except:
+    pass
